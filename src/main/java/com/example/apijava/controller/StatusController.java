883008a12258c9dc18c5134e7bd6670871a32dc6
Nascimento <rafaelnascimentodelimarnl@gmail.com>
2022-07-21 16:50:27 -0300

    API JSM 1.0

---
+++ src/main/java/com/example/apijava/controller/StatusController.java
@@ -0,0 +1,15 @@
+package com.example.apijava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class StatusController {
+
+    @GetMapping(path = "/api/status")
+    public String check() {
+        return "online Mysql server + springframework";
+    }
+
+}
